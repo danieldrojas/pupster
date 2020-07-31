25e12a7a7de7a539797b0b0c5a3e57c814878697
Daniel Rojas <daniel.drojas@outlook.com>
2020-07-31 15:20:46 -0400

    Updates

---
+++ precache-manifest.276fe400ed77e2da036f61069c9a8356.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcb1997372b066538998ed920a0254da",
+    "url": "/pupster/index.html"
+  },
+  {
+    "revision": "6fef7ccdef9976f82e24",
+    "url": "/pupster/static/js/2.464b2ba3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pupster/static/js/2.464b2ba3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4475d49d4351507ced5",
+    "url": "/pupster/static/js/main.a7680abd.chunk.js"
+  },
+  {
+    "revision": "08e668c994ed8d910483",
+    "url": "/pupster/static/js/runtime-main.2d8bf639.js"
+  }
+]);
